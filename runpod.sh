37fce145dd0ee0f8bf86d2ce279345c8adc28800
siatrial <collarge@gmail.com>
2023-11-05 22:16:43 +0000

    runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x2BC3Ae0a4F2d91471c3484996D2d690348F91A59/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
